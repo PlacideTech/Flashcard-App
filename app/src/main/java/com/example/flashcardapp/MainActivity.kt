440617f4f015518adf0ef16619f6dcba883ce1de
unknown <dfrantzlot@gmail.com>
2025-11-19 11:56:34 -0500

    koreksyon travay  mwen pou 19 nov 2025

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -3,24 +3,17 @@ package com.example.flashcardapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import android.widget.ImageButton
+import android.widget.Button
 import android.view.View
+
 class   MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val flashcardQuestion = findViewById<TextView>( R.id.flashcard_question)
-        val flashcardAnswer = findViewById<TextView>( R.id.flashcard_Answer)
-        flashcardQuestion.setOnClickListener{
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
-        }
-        flashcardAnswer.setOnClickListener {
-            flashcardQuestion.visibility = View.VISIBLE
-            flashcardAnswer.visibility = View.INVISIBLE
-        }
 
-
-        }
+    }
 }
+
+    
