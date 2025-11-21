161aa31ca700d967c1bd210ccc08d9aead4ef51e
unknown <dfrantzlot@gmail.com>
2025-11-21 15:27:57 -0500

    travay mwen pou jodi an 21 nov 2025

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.flashcardapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Button
 import android.view.View
 
@@ -12,6 +12,20 @@ class   MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>( R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>( R.id.flashcard_answer)
+        val flashcardAnswer1 = findViewById<TextView>(R.id.flashcard_answer1)
+        val flashcardAnswer2 = findViewById<TextView>(R.id.flashcard_answer3)
+        val btsView = findViewById<Button>(R.id.BTS)
+
+        flashcardAnswer.setOnClickListener{
+            findViewById<View>(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.my_red_color, null))
+            findViewById<View>(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.my_Blanc_color, null))
+            findViewById<View>(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.my_vert_color, null))
+        }
+
+
+
 
     }
 }
